5daa67a45c127482d4ed78de811ee1a73b5ed987
Roxana Leontie <roxana@gwu.edu>
2019-02-15 10:04:38 -0500

    Added template for schema creation

---
+++ GWNetID_store_schema.sql
@@ -0,0 +1,7 @@
+-- add drop table for all your tables if they exist
+-- DROP TABLE IF EXISTS table_name CASCADE;
+
+
+-- add create table for all your tabled
+
+-- add insert statements to populate your tables
